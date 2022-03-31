bf625593f9437af2639e8fc8ecc63c43e13594c4
hongho092 <93205420+hongho092@users.noreply.github.com>
2022-03-31 22:03:39 +0700

    first commit

--- case_study/case_study_1/task_2_to_20.sql
+++ case_study/case_study_1/task_2_to_20.sql
@@ -1,9 +1,9 @@
 use case_study_module3;
 
 -- 2
-select nhan_vien.* from nhan_vien
-where (nhan_vien.ho_ten like "H%" or nhan_vien.ho_ten like "T%" or nhan_vien.ho_ten like "K%") 
-and char_length(nhan_vien.ho_ten) < 15;
+select * from khach_hang kh
+where (year(now()) - year(kh.ngay_sinh) > 18 and year(now()) - year(kh.ngay_sinh) < 50)
+and (kh.dia_chi like "%Đà Nẵng" or kh.dia_chi like "%Quảng Trị");
 
 -- 3
 select khach_hang.* from khach_hang
@@ -11,47 +11,61 @@ where (year(now()) - year(khach_hang.ngay_sinh) > 18 and year(now()) - year(khac
 and (khach_hang.dia_chi like "%Đà Nẵng" or khach_hang.dia_chi like "%Quảng Trị"); 
 
 -- 4
-select khach_hang.ma_khach_hang, khach_hang.ho_ten, count(hop_dong.ma_hop_dong) as so_lan_dat
-from khach_hang
-join loai_khach_hang on khach_hang.ma_loai_khach = loai_khach_hang.ma_loai_khach
-join hop_dong on khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
-where loai_khach_hang.ten_loai_khach = "Diamond"
-group by khach_hang.ma_khach_hang
-order by so_lan_dat;
+select kh.ma_khach_hang, kh.ho_ten, count(hd.ma_khach_hang) as so_lan_dat_phong
+from khach_hang kh 
+join hop_dong hd on kh.ma_khach_hang = hd.ma_khach_hang
+join loai_khach_hang lkh on kh.ma_loai_khach = lkh.ma_loai_khach
+where ten_loai_khach = "Diamond"
+group by kh.ho_ten
+order by  so_lan_dat_phong;
 
 -- 5
-select khach_hang.ma_khach_hang, khach_hang.ho_ten, loai_khach_hang.ten_loai_khach, hop_dong.ma_hop_dong, dich_vu.ten_dich_vu, hop_dong.ngay_lam_hop_dong, hop_dong.ngay_ket_thuc, 
-       if (hop_dong_chi_tiet.so_luong is null, dich_vu.chi_phi_thue, sum(dich_vu.chi_phi_thue + (hop_dong_chi_tiet.so_luong * dich_vu_di_kem.gia))) as tong_tien
-from khach_hang
-left join loai_khach_hang on khach_hang.ma_loai_khach = loai_khach_hang.ma_loai_khach
-left join hop_dong on khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
-left join dich_vu on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
-left join hop_dong_chi_tiet on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
-left join dich_vu_di_kem on hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
-group by hop_dong.ma_hop_dong;
+select kh.ma_khach_hang, kh.ho_ten, lkh.ten_loai_khach, hd.ma_hop_dong, dv.ten_dich_vu, hd.ngay_lam_hop_dong, hd.ngay_ket_thuc, 
+       if (hdct.so_luong is null, dv.chi_phi_thue, sum(dv.chi_phi_thue + (hdct.so_luong * dvdk.gia))) as tong_tien
+from khach_hang kh
+left join loai_khach_hang lkh on kh.ma_loai_khach = lkh.ma_loai_khach
+left join hop_dong hd on kh.ma_khach_hang = hd.ma_khach_hang
+left join dich_vu dv on hd.ma_dich_vu = dv.ma_dich_vu
+left join hop_dong_chi_tiet hdct on hd.ma_hop_dong = hdct.ma_hop_dong
+left join dich_vu_di_kem dvdk on hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+group by hd.ma_hop_dong;
 
 -- 6
-select dich_vu.ma_dich_vu, dich_vu.ten_dich_vu, dich_vu.dien_tich, dich_vu.chi_phi_thue, loai_dich_vu.ten_loai_dich_vu
-from dich_vu
-inner join loai_dich_vu on dich_vu.ma_loai_dich_vu = loai_dich_vu.ma_loai_dich_vu
-where dich_vu.ten_dich_vu 
-not in (
-select dich_vu.ten_dich_vu 
-from dich_vu 
-inner join hop_dong on dich_vu.ma_dich_vu = hop_dong.ma_dich_vu
-where hop_dong.ngay_lam_hop_dong
-between "2021-01-01" AND "2021-03-31")
-order by dich_vu.dien_tich desc;
+select dv.ma_dich_vu, dv.ten_dich_vu, dv.dien_tich, dv.chi_phi_thue, ldv.ten_loai_dich_vu
+from dich_vu dv
+join loai_dich_vu ldv on dv.ma_loai_dich_vu = ldv.ma_loai_dich_vu
+where dv.ma_dich_vu
+not in (select dv.ma_dich_vu 
+		from dich_vu dv 
+        join hop_dong hd on hd.ma_dich_vu = dv.ma_dich_vu 
+        where hd.ngay_lam_hop_dong between "2021/01/01" and "2021/03/30")
+order by dv.dien_tich desc;
 
 -- 7
-
+select dv.ma_dich_vu, dv.ten_dich_vu, dv.dien_tich, dv.so_nguoi_toi_da, dv.chi_phi_thue, ldv.ten_loai_dich_vu
+from dich_vu dv
+join loai_dich_vu ldv on dv.ma_loai_dich_vu = ldv.ma_loai_dich_vu
+where dv.ma_dich_vu 
+in (select dv.ma_dich_vu
+	from dich_vu dv
+    join hop_dong hd on dv.ma_dich_vu = hd.ma_dich_vu
+    where hd.ngay_lam_hop_dong between "2020/01/01" and "2020/12/31") 
+and dv.ma_dich_vu 
+not in (select dv.ma_dich_vu
+	from dich_vu dv
+    join hop_dong hd on dv.ma_dich_vu = hd.ma_dich_vu
+    where hd.ngay_lam_hop_dong between "2021/01/01" and "2021/12/31");
+ 
 -- 8
-select nhan_vien.ho_ten from nhan_vien
+select ho_ten from nhan_vien
 group by ho_ten;
-
+select ho_ten from nhan_vien
+union
+select ho_ten from nhan_vien;
 select distinct ho_ten from nhan_vien;
 
 -- 9
+
 
 
 
