04ec2dfdc314b43351e68ae439c1dec1f220fa9e
hongho092 <93205420+hongho092@users.noreply.github.com>
2022-03-31 11:08:03 +0700

    first commit

--- ss03_Thao tác với CSDL/bt_thao_tac_voi_CSDL_quan_li_ban_hang.sql
+++ ss03_Thao tác với CSDL/bt_thao_tac_voi_CSDL_quan_li_ban_hang.sql
@@ -26,6 +26,20 @@ value (1, 1, 3),
       (2, 5, 4),
       (2, 3, 3);  
       
-select * from order_detail;
+select oID, oDate, oTotalPrice from `order`;
 
-select customer.cName, 
+select customer.cID, customer.cName, `order`.cID, product.pName
+from customer 
+join `order` on customer.cID = `order`.cID
+join order_detail on `order`.oID = order_detail.oID
+join product on order_detail.pID = product.pID;
+
+select customer.cName 
+from customer
+where customer.cName not in (select customer.cName from customer join `order` on customer.cID = `order`.cID group by customer.cName);
+
+select order_detail.oID, `order`.oDate, sum(order_detail.odQTY * product.pPrice) as price_order
+from `order`
+join order_detail on `order`.oID = order_detail.oID
+join product on order_detail. pID = product.pID
+group by oDate;
